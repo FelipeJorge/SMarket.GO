750016292c28f5b4025be5af4f9ec853de228b75
fehj <felipejorgecd@gmail.com>
2022-12-02 21:06:08 -0300

    add form list

--- smarketgo/src/App.js
+++ smarketgo/src/App.js
@@ -1,9 +1,52 @@
 import './App.css';
+import {useState, useEffect} from 'react'
+import {Bstrash, BsbookmarkCheck, BsbookmarkCheckFill} from 'react-icons/bs'
+
+
+const API = "http://localhost:5000"
+
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [tobuy, setTobuy] = useState([]);
+  const [loading, setLoading] = useState (false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitle("");
+    console.log("enviou");
+    console.log(title)
+  }
+
   return (
     <div className="App">
-      <h2>testando</h2>
+      <div className='tobuy-header'>
+        <h1>List to buy</h1>
+      </div>
+      <div className='form-tobuy'>
+        <h2>Adicione um item a Lista</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='title'>Produto</label>
+            <input 
+            type="text" 
+            name='title'
+            placeholder='Ex:Arroz' 
+            onChange={(e) => setTitle(e.target.value)}
+            value={title || ""}
+            required
+            />
+             
+          </div>
+          <input type="submit" value="adicionar"/>
+        </form>
+      </div>
+      <div className='list-tobuy'>
+        <h2>Lista de Compras:</h2>
+        {tobuy.length === 0 && <p>Lista vazia</p>}
+      </div>
+      
     </div>
     
   );
